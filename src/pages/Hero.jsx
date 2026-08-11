107b1b359504762e847c2bf0ce3e1a75221a0507
Sharvari <sharvarisapte22@gmail.com>
2026-08-11 20:11:39 +0530

    regibtn

--- src/pages/Hero.jsx
+++ src/pages/Hero.jsx
@@ -52,7 +52,7 @@ const Hero = () => {
   useEffect(() => {
     const timer = setTimeout(() => {
       setMinTimePassed(true);
-    }, 3000);
+    }, 0);
 
     return () => clearTimeout(timer);
   }, []);
@@ -118,7 +118,7 @@ const Hero = () => {
         )}
 
         {loaded && (
-          <div className='absolute top-[28dvh] md:top-[20vh] md:left-[10vw] lg:left-[17vw] w-[clamp(300px,40%,400px)]'>
+          <div className='absolute top-[28dvh] md:top-[17vh] md:left-[10vw] lg:left-[17vw] w-[clamp(300px,40%,400px)]'>
             <div className='flex flex-col gap-4 items-center'>
               <img src="/Heading.png" alt="Heading" />
               <div className='text-[#e8dfce] flex w-[90%] justify-between items-baseline font-mono'>
@@ -150,7 +150,25 @@ const Hero = () => {
                 ))}
               </div>
             </div>
-          </div>
+                <div>
+          <a 
+              href="https://docs.google.com/forms/d/e/1FAIpQLSfDWzX2pvKTyXPa3YoZtdUhdbMJvKH1NNbghtDqtY-YlycBvw/viewform" 
+              target="_blank" 
+              rel="noopener noreferrer"
+              className=" scale-70 group relative inline-flex items-center justify-center 
+              w-full max-w-[300px] sm:max-w-[380px] py-6 px-8 
+              text-center bg-gradient-to-r from-[#8b6e22] via-[#d7a832] to-[#8b6e22] hover:from-[#d7a832]
+               hover:via-[#e7cfa3] hover:to-[#d7a832] text-[#15120f] font-['Black_Ops_One'] text-lg md:text-xl tracking-[0.2em] uppercase font-bold border-2 
+               border-[#e7cfa3] shadow-[0_0_25px_rgba(215,168,50,0.5)] hover:shadow-[0_0_40px_rgba(231,207,163,0.85)] transition-all duration-300 transform active:scale-[0.98] 
+               focus:outline-none focus:ring-4 focus:ring-[#d7a832]/60 cursor-pointer rounded-sm"
+              aria-label="Register Now for Invictus 1.0 Recruitment"
+            >
+              <span className="mr-2 text-xl group-hover:translate-x-0.5 transition-transform duration-200">★</span>
+              Register Now
+              <span className="ml-2 text-xl group-hover:translate-x-1 transition-transform duration-200">➔</span>
+            </a>
+            </div>
+                 </div>
         )}
 
         <div className="absolute bottom-0 left-0 w-full h-10 md:h-10 bg-gradient-to-t from-[#0d0b09] via-[#0d0b09]/80 to-transparent pointer-events-none z-10" />
